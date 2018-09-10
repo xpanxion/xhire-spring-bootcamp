0518044cd1a8c4aa57e5ec19695b227f0541b022
Brian Smith <bsmith@xpanxion.com>
2018-09-10 15:49:44 -0500

    Inital Commit

---
+++ jenkins/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo yum -y update
+sudo yum -y install wget java-1.8.0-openjdk
+
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+
+sudo yum -y install jenkins
+sudo service jenkins start
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
